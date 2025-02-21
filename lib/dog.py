ada0512c8fa508cc5274f70625022a43b33f30ca
eddy <eddy.mwaniki.27@gmail.com>
2025-02-22 02:03:36 +0300

    init and self

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name 
+        self.breed = breed    
+    def introduction(self):
+      print(f"My name is {self.name}. I am a {self.breed}")  
+
+dog1 = Dog("Scooby")   
+dog2 = Dog("Jewel", "Dalmatian")
+dog3 = Dog("Bolt", "German Shepherd")
+
+print(dog1.breed)
+print(dog2.breed)
+print(dog3.name)
+dog1.introduction()
+dog2.introduction()
+dog3.introduction()
